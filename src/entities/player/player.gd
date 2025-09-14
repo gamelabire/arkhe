5030ed96a41cf55d8b2377b06359808d32a64464
yudur <duarteyudi218@gmail.com>
2025-09-14 19:31:45 -0300

    basic cutscene for level 1 created

--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var is_in_cutscene: bool = false
 var _is_attacking: bool = false
 var _state_machine
 
@@ -11,21 +12,39 @@ var _state_machine
 @export_category("Objects")
 @export var _animation_tree: AnimationTree
 @export var attack_timer: Timer
+@export var camera: Camera2D
 
+@export_category("Camera")
+@export var limit_camera_top = -10000000
+@export var limit_camera_bottom = 10000000
+@export var limit_camera_right = 10000000
+@export var limit_camera_left = -10000000
 
 
 func _ready() -> void:
 	_animation_tree = $AnimationTree
 	_state_machine = _animation_tree.get("parameters/playback")
+	
 	attack_timer = $AttackTimer
-
+	
+	# changing the limits of the player's camera as far as he can see on the map
+	camera = $Camera2D
+	camera.limit_top = limit_camera_top
+	camera.limit_bottom = limit_camera_bottom
+	camera.limit_right = limit_camera_right
+	camera.limit_left = limit_camera_left
 
 func _physics_process(delta: float) -> void:
+	if is_in_cutscene:
+		# In cutscene mode, animation is controlled externally (by AnimationPlayer)
+		# and there is no movement per input.
+		move_and_slide()
+		return
+	
 	_move(delta)
 	_attack()
 	_animate()
 	move_and_slide()
-
 
 func _move(delta: float) -> void:
 	var direction_player := Vector2(
@@ -47,17 +66,13 @@ func _move(delta: float) -> void:
 	else:
 		velocity.x = lerp(velocity.x, direction_player.x * _move_speed, _friction * delta)
 		velocity.y = lerp(velocity.y, direction_player.y * _move_speed, _friction * delta)
-	
-	
+
 func _attack():
 	if Input.is_action_just_pressed("attack") and not _is_attacking:
 		set_physics_process(false)
 		attack_timer.start()
 		_is_attacking = true
-	
-	
-		
-		
+
 func _animate() -> void:
 	if _is_attacking:
 		_state_machine.travel("attack")
@@ -66,7 +81,10 @@ func _animate() -> void:
 	else:
 		_state_machine.travel("idle")
 
-
 func _on_attack_timer_timeout() -> void:
 	set_physics_process(true)
 	_is_attacking = false
+
+func play_anim(state: String, direction: Vector2):
+	_animation_tree["parameters/%s/blend_position" % state] = direction
+	_state_machine.travel(state)
